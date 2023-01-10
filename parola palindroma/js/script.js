ebdfe21028081da3d907182230ea16ffd21804d5
Francesco98Welcome <frankbenvenuti04@gmail.com>
2023-01-10 18:39:54 +0100

    end es.

--- parola palindroma/js/script.js
+++ parola palindroma/js/script.js
@@ -1,11 +1,14 @@
 let inserisciUnaParola = prompt('Inserisci una parola');
 let parolaInversa = invertiParola(inserisciUnaParola);
+let h2 = document.querySelector('h2');
 
 if(inserisciUnaParola == parolaInversa){
     console.log('la parola è palindroma');
-  } else {
+    h2.innerHTML = 'la parola è palindroma';
+} else {
     console.log('la parola non è palindroma');
-  }
+    h2.innerHTML ='la parola non è palindroma';
+}
   
 function invertiParola(str){
   let strInversa = str.split('').reverse().join('');  
